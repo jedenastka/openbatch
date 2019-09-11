f8a993b8655000a66f2b891ddbbaf9f210cb0063
Grzesiek11 <grzesiek.jedenastka@outlook.com>
2019-09-11 17:54:50 +0200

    Moved the execution to another function.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 
+class Enviroment {};
+
 std::vector<std::string> split(std::string string, char splitChar) {
     std::vector<std::string> splitedString;
     std::string currentSubstring;
@@ -22,13 +24,22 @@ std::vector<std::string> split(std::string string, char splitChar) {
 template <typename Vector>
 void printVector(Vector vector, std::string comma = " ", std::string end = "\n") {
     int size = vector.size();
-    for (int i = 0; i < size; i++) {
-        std::cout << vector[i];
-        if (i < size -1) {
-            std::cout << comma;
+    if (size > 1) {
+        for (int i = 0; i < size; i++) {
+            std::cout << vector[i];
+            if (i < size -1) {
+                std::cout << comma;
+            }
         }
+        std::cout << end;
     }
-    std::cout << end;
+}
+
+void execute(std::string command, std::vector<std::string> args) {
+    if (1) {
+        std::cout << command << ' ';
+        printVector(args);
+    }
 }
 
 int main() {
@@ -36,9 +47,8 @@ int main() {
         std::string line;
         getline(std::cin, line, '\n');
         std::vector<std::string> args = split(line, ' ');
-        std::cout << line << '\n';
         std::string command = args[0];
         args.erase(args.begin());
-        printVector(args);
+        execute(command, args);
     }
 }
